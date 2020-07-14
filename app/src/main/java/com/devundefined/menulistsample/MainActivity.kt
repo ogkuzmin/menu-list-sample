ed93e7a7a6717b2241af5580d17924d6de37032d
Oleg Kuzmin <o.kuzmin@dodopizza.com>
2020-07-14 23:11:37 +0300

    Effect of splash screen

--- app/src/main/java/com/devundefined/menulistsample/MainActivity.kt
+++ app/src/main/java/com/devundefined/menulistsample/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.devundefined.menulistsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
