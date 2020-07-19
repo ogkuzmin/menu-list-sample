25b4ef3883727f47abcfb8d45d0f173d2a2802b3
Oleg Kuzmin <o.kuzmin@dodopizza.com>
2020-07-19 21:32:35 +0300

    Fix UI. Cards for recycler

--- app/src/main/java/com/devundefined/menulistsample/presentation/productlist/ProductListFragment.kt
+++ app/src/main/java/com/devundefined/menulistsample/presentation/productlist/ProductListFragment.kt
@@ -1,9 +1,11 @@
 package com.devundefined.menulistsample.presentation.productlist
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devundefined.menulistsample.R
 import com.devundefined.menulistsample.databinding.FragmentProductListBinding
@@ -12,6 +14,7 @@ import com.devundefined.menulistsample.domain.models.Product
 class ProductListFragment : Fragment(R.layout.fragment_product_list) {
 
     companion object {
+        private const val COLUMNS_COUNT = 2
         private const val EXTRA_KEY_PRODUCT_LIST = "EXTRA_KEY_PRODUCT_LIST"
         fun newInstance(products: Array<Product>): ProductListFragment =
             ProductListFragment().apply {
@@ -28,7 +31,11 @@ class ProductListFragment : Fragment(R.layout.fragment_product_list) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProductListBinding.bind(view)
         binding.recycler.run {
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                LinearLayoutManager(requireContext())
+            } else {
+                GridLayoutManager(requireContext(), COLUMNS_COUNT)
+            }
             adapter = ProductAdapter(
                 arguments?.getParcelableArray(EXTRA_KEY_PRODUCT_LIST) as? Array<Product>
                     ?: emptyArray()
